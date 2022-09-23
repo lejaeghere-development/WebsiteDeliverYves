6ff4908e070b775603443b5ab0bd3f5cf298cb09
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2022-09-23 15:57:50 +0200

    add btw number and company name twikey

--- js/twikey.js
+++ js/twikey.js
@@ -1,21 +1,18 @@
 
 
 function getLogin() {
-  //const errorMessage = document.getElementById("errorMessage");
-  //const errorDiv = document.getElementById("errorDiv");
   const iban = document.getElementById("iban").value;
   const email = document.getElementById("email").value;
   const lastname = document.getElementById("lastname").value;
   const firstname = document.getElementById("firstname").value;
+  const vatno = document.getElementById("vatno").value;
+  const companyName = document.getElementById("companyName").value;
   const phone = document.getElementById("phone").value;
   const address = document.getElementById("address").value;
   const city = document.getElementById("city").value;
   const zip = document.getElementById("zip").value;
   const url = "https://twikey-deliveryves.herokuapp.com";
-  // if (!lastname) {
-  //   errorDiv.style.display = "block";
-  //   errorMessage.innerHTML = "Vul uw naam correct in";
-  // }
+
 
   var requestOptions = {
     method: "GET",
@@ -23,17 +20,20 @@ function getLogin() {
   };
 
   fetch(`${url}/api/twikey/login`, requestOptions)
+
     .then(function(response) {
       const data = response.json();
-      console.log(data);
       return data;
     })
     .then(function(data) {
+      console.log(data.Authorization);
       const apiToken = data.Authorization;
       let parsedata = {
         iban,
         email,
         lastname,
+        vatno,
+        companyName,
         phone,
         address,
         firstname,
@@ -50,12 +50,13 @@ function getLogin() {
         body: JSON.stringify(parsedata)
       };
 
+      console.log(parsedata);
       fetch(`${url}/api/twikey/nieuweklant`, requestOptions)
+      
         .then(response => response.json())
-        .then(result => (window.location.href = result)) // (window.location.href = result))
+        .then(result => (console.log(result)))
+        .then(result => (window.location.href  = result))
         .catch(error => console.log("error", error));
     })
     .catch(error => console.log("error", error));
-  
 };
-
